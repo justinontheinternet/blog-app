aa12dc97071b075e6cc8a9c4408242f671661efc
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-05-11 23:37:15 -0400

    tied fetchPosts action to app

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
-    
+    this.props.fetchPosts();
   }
 
   render() {
@@ -14,4 +17,8 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
